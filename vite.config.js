9d309efceddbd9b465d7623a47a4fdef6c4c56fd
mohemohe <mohemohe@ghippos.net>
2023-07-05 05:03:22 +0900

    vendorチャンク分割を有効にする

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
@@ -31,6 +31,7 @@ export default defineConfig(({ mode }) => {
           },
         },
       }),
+      splitVendorChunkPlugin(),
     ],
   };
 });
